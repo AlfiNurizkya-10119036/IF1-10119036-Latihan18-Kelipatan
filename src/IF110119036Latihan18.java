0ae9d3f2d95d3df1742d540a7624d1683142f804
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-28 20:32:15 +0700

    Project 18

---
+++ src/IF110119036Latihan18.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * Nama  : ALFI NURIZKYA
+ * Kelas : IF-1
+ * NIM   : 10119036
+ * Deskripsi Program : Program Kelipatan
+ */
+
+import java.util.Scanner;
+
+public class IF110119036Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        Scanner input = new Scanner(System.in);
+      
+        double j = 3.5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i*j);
+        }
+    }
+    
+}
